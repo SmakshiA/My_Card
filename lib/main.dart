519751229f2cb09a7d5b886e7a0fa80723e5d83f
smakshi <smakshina1306@gmail.com>
2024-05-06 10:16:42 +0530

    Basic body done

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal.shade300,
-        body: Container(),
+        backgroundColor: Colors.deepPurple.shade100,
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: CircleAvatar(
+                  child: Image(image: AssetImage('images/smakshi.png')),
+                  radius: 100.0,
+                  backgroundColor: Colors.white,
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Smakshi A',
+
+                ),
+                height: 30.0,
+                alignment: Alignment.center,
+              ),
+              Container(
+                child: Text('Flutter Developer'),
+                alignment: Alignment.center,
+              ),
+              Container(
+                child: Text('8856827789'),
+                alignment: Alignment.center,
+                color: Colors.white,
+                height: 45.0,
+                margin: EdgeInsets.fromLTRB(70, 5, 70, 5),
+                // padding: EdgeInsets.only(left: 20),
+              ),
+              Container(
+                child: Text('smakshina1306@gmail.com'),
+                alignment: Alignment.center,
+                color: Colors.white,
+                height: 45.0,
+                margin: EdgeInsets.fromLTRB(70, 5, 70, 5),
+                // padding: EdgeInsets.only(left: 20),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  hello(){}
 }
-
-
